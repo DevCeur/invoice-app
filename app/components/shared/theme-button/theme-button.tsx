c772bbc85da3a1cb121e0eb7c6e051fe90749491
Carlos Umaña <dev.carlos.umana@gmail.com>
2022-12-06 10:10:13 -0500

    [feat]: improve theme button

--- app/components/shared/theme-button/theme-button.tsx
+++ app/components/shared/theme-button/theme-button.tsx
@@ -1,3 +1,4 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import { BiMoon, BiSun } from 'react-icons/bi';
 
 import { THEME } from '~/utils/enum';
@@ -18,7 +19,29 @@ export const ThemeButton = () => {
       onClick={handleToggleTheme}
       className="w-12 h-12 text-2xl flex justify-center items-center bg-overlay-light-background hover:bg-gray-200 dark:bg-overlay-dark-background hover:dark:bg-purple-darkest rounded-full"
     >
-      {theme === 'dark' ? <BiSun /> : <BiMoon />}
+      <AnimatePresence initial={false} mode="wait">
+        {theme === THEME.DARK ? (
+          <motion.span
+            key="sun"
+            initial={{ opacity: 0, translateY: -5 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            exit={{ opacity: 0, translateY: 5 }}
+            transition={{ duration: 0.2 }}
+          >
+            <BiSun />
+          </motion.span>
+        ) : (
+          <motion.span
+            key="moon"
+            initial={{ opacity: 0, translateY: -5 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            exit={{ opacity: 0, translateY: 5 }}
+            transition={{ duration: 0.2 }}
+          >
+            <BiMoon />
+          </motion.span>
+        )}
+      </AnimatePresence>
     </button>
   );
 };
